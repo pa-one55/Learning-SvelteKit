fe2ca26d9ab09d4b149101a4528b2aac9d3cf4e3
Pa.One <pkpawankumar2806@gmail.com>
2024-07-05 12:49:03 +0530

    bug fix /about to /About

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,7 +1,7 @@
 
 <nav class="bg-gray-900 py-4 shadow-md border-b-2 border-gray-800">
   <div class="container mx-auto flex justify-between items-center">
-    <a href="/about" class="text-xl font-bold text-gray-100 px-4 py-2 rounded-md bg-gray-700 hover:bg-gray-800 transition duration-300">About Pa One</a>
+    <a href="/About" class="text-xl font-bold text-gray-100 px-4 py-2 rounded-md bg-gray-700 hover:bg-gray-800 transition duration-300">About Pa One</a>
 
     <div class="hidden md:flex space-x-4">
       <a href="/" class="text-gray-100 hover:text-white">Home</a>
